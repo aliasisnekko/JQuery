818f8bcfa84825c39615c4797883a1ef646ea2a7
Nekko Street <clayvon.street@gmail.com>
2021-01-10 18:29:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$('h1').addClass('big-title');
+$("a").attr('href', 'https://yahoo.com')
+$('button').addClass('btn-danger');
+$("button").html('<b>Dont Click Me</b>')
+
+$(document).keydown(function(event) {
+    $("h1").text(event.key);
+})
+$("h1").on('click', function() {
+    $("h1").css('color', "purple");
+})
+
+$('button').on('click', function() {
+    $('h1').css('color', "red");
+})
+$('button').on('click', function() {
+    $('h1').slideToggle().animate({ opacity: 0.5 });
+})
