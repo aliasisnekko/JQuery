cb41df6b871aa28b1ea23886018100e32a6619c9
Nekko Street <clayvon.street@gmail.com>
2021-02-16 21:22:05 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $('h1').addClass('big-title');
 $("a").attr('href', 'https://yahoo.com')
+$("a").css('color', 'white')
 $('button').addClass('btn-danger');
 $("button").html('<b>Dont Click Me</b>')
+$('button').on('click', function() {
+    $('h1').css('color', "red");
+    $('button').html('<b>I said dont click me</b>');
+    $('h1').slideToggle();
 
+});
 $(document).keydown(function(event) {
     $("h1").text(event.key);
 })
@@ -10,11 +16,6 @@ $("h1").on('click', function() {
     $("h1").css('color', "purple");
 })
 
-$('button').on('click', function() {
-    $('h1').css('color', "red");
-    $('button').html('<b>I said dont click me</b>');
-    $('h1').slideToggle();
-})
-$('button').on('click', function() {
-    $('h1').slideToggle().animate({ opacity: 0.5 });
-})
+
+
+$('h1').addClass('text-center');
